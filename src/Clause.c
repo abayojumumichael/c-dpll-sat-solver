0e571ec6a50191cfa649f74fe73f67cb68000f3a
Michael Abayomi-Ojumu <abayojumumichael@gmail.com>
2026-02-20 11:24:17 +0000

    Implemented new_Clause

---
+++ src/Clause.c
@@ -0,0 +1,12 @@
+/*
+ * Source file for implementation of the Clause declared in Clause.h
+ */
+
+#include "Clause.h"
+
+// TODO: Implement all the the functions declared in the Clause.h interface
+Clause new_Clause() {
+    Clause clause = {.size = 0};
+    return clause;
+}
+
